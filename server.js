da9ab5a72c3866ab25bcb24b684fb972cfef0ab1
Chandra Reddy <vgolugur@cisco.com>
2018-07-08 15:33:59 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+"/views/partials");
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `Date : ${now} : ${req.method} ${req.url} `
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err) => {
+        console.log('testing..');
+    });
+    next();
+});
+
+// app.use((req,res,next) => {
+//     res.render("maintenance.hbs",{
+//
+//     });
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+    res.render("home.hbs",{
+        pageTitle : 'About Page',
+        welcomeMsg: 'Welcome Chandra'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render("about.hbs",{
+        pageTitle : 'About Page',
+    });
+});
+
+app.listen(3000,() => {
+    console.log("Server is up ");
+});
