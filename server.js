e8f7987f8e17650cc7305726750e56e1dc7bd939
Chandra Reddy <vgolugur@cisco.com>
2018-07-11 15:21:20 +0530

    changes for adding project page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about',(req,res) => {
     });
 });
 
+app.get('/projects',(req,res) => {
+    res.render("projects.hbs",{
+        pageTitle : 'Projects Page',
+        welcomeMsg: 'projects Page message'
+    });
+});
+
 app.listen(port,() => {
     console.log(`Server is up on ${port}`);
 });
